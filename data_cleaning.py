95f12dc59984a289626a7c66df2b60ee35c6f8a1
shivainlabs <shiva.in.labs@gmail.com>
2026-05-17 09:04:05 +0530

    Update url

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-url = "https://docs.google.com/spreadsheets/d/1ZyTt2VrhFFk8ro_E8juDT1KNSMQpgkXWJAxwPNj0Sr4/export?format=csv&gid=0"
-
+# url = "https://docs.google.com/spreadsheets/d/1ZyTt2VrhFFk8ro_E8juDT1KNSMQpgkXWJAxwPNj0Sr4/export?format=csv&gid=0"
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQVpCawkt66Cfv04xQmI1QZ_c8paOKSupUxAP_Uf-K7fee-UBQeE9GKiU_7XURlPU6J0fHoMd9Wsw5w/pub?output=csv"
 
 def load_and_clean_data():
     df = pd.read_csv(url)
